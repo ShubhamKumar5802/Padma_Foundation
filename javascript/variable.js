c65b657f14bf8536d22c2bf3b8094c8a217f5a32
Shubham <sktovisit@gmail.com>
2021-09-24 01:07:19 +0530

    slider_added

---
+++ javascript/variable.js
@@ -0,0 +1,8 @@
+// header 
+const navItems = document.querySelectorAll(".header__container__list--item");
+
+// Slides
+const slides = document.querySelectorAll('.slider__slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
